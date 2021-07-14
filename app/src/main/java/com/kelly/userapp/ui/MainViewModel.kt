8c3a00b265f4de6e912a801605e7ca5a5a4543e7
kelienzo <kellyefone@gmail.com>
2021-07-14 14:12:11 +0100

    Update commit

--- app/src/main/java/com/kelly/userapp/ui/MainViewModel.kt
+++ app/src/main/java/com/kelly/userapp/ui/MainViewModel.kt
@@ -4,26 +4,20 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.kelly.userapp.api.RetroFitProvider
 import com.kelly.userapp.models.User
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 
     val usersLiveData = MutableLiveData<List<User>>()
 
-    fun getUsers(){
-        RetroFitProvider.placeHolderAPI.getUsers().enqueue(object: Callback<List<User>> {
-            override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
-                response.body()?.let {
-                    usersLiveData.postValue(it)
-                }
-            }
-
-            override fun onFailure(call: Call<List<User>>, t: Throwable) {
-                TODO("Not yet implemented")
-            }
-
-        })
+    fun getUsers() {
+        CoroutineScope(Dispatchers.IO).launch {
+            usersLiveData.postValue(RetroFitProvider.placeHolderAPI.getUsers())
         }
     }
+}
